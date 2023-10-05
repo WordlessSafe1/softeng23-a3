dfc938a55f86ee0a5ac41c2c49124ffac78eacdc
Connor Cummings <94203733+WordlessSafe1@users.noreply.github.com>
2023-10-05 14:39:11 -0500

    Add tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 import pytest
+from pathlib import Path
 
-from pmgr.project import Project
+from pmgr.project import Project,TaskException
 
 @pytest.fixture(scope="function")
 def testproj():
@@ -12,3 +13,23 @@ def test_add(testproj):
     testproj.add_task('dosomething')
     assert 'dosomething' in testproj.get_tasks()
 
+def test_add_duplicate(testproj):
+    testproj.add_task('dosomething')
+    with pytest.raises(TaskException):
+        testproj.add_task('dosomething')
+
+def test_remove_task(testproj):
+    testproj.add_task('something')
+    testproj.remove_task('something')
+    assert 'something' not in testproj.get_tasks()
+
+def test_remove_inexistent_task(testproj):
+    with pytest.raises(TaskException):
+        testproj.remove_task('something')
+    return 
+
+def test_write_after_remove(testproj):
+    testproj.add_task('something')
+    testproj.add_task('somethingelse')
+    testproj.remove_task('somethingelse')
+    assert True
